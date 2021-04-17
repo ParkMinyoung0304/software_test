ca3d3398930b290ceeb820c0f9d851cbce028cae
ParkMinyoung0304 <62382458+ParkMinyoung0304@users.noreply.github.com>
2021-04-17 14:53:45 +0800

    Update test_parametrize.py

--- test_parametrize.py
+++ test_parametrize.py
@@ -2,23 +2,23 @@ import pytest
 def add_function(a,b):
     return a+b
 
-# def test1_add1():
-#     assert add_function(1,1)==2
-# def test1_add2():
-#     assert add_function(1,-1)==0
-# def test1_add3():
-#     assert add_function(1,-1)==-2
-# def test1_add4():
-#     assert add_function(1000,10000)==2
+def test1_add1():
+    assert add_function(1,1)==2
+def test1_add2():
+    assert add_function(1,-1)==0
+def test1_add3():
+    assert add_function(1,-1)==-2
+def test1_add4():
+    assert add_function(1000,10000)==2
 
 # 参数化，参数加别名，ids
-# @pytest.mark.parametrize("a,b,expected",
-#                         [(3,5,8),
-#                         (-1,-3,-4),
-#                         (10000,1000,11000),
-#                         ],ids=["int","minus","bigint"])
-# def test_add(a,b,expected):
-#     assert add_function(a,b) == expected
+@pytest.mark.parametrize("a,b,expected",
+                        [(3,5,8),
+                        (-1,-3,-4),
+                        (10000,1000,11000),
+                        ],ids=["int","minus","bigint"])
+def test_add(a,b,expected):
+    assert add_function(a,b) == expected
 
 
 # 参数排列组合
