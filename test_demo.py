9b5380f8adf45d5b5c635da24c28290530d13e1a
ParkMinyoung0304 <62382458+ParkMinyoung0304@users.noreply.github.com>
2021-04-17 14:49:36 +0800

    Add files via upload

---
+++ test_demo.py
@@ -0,0 +1,8 @@
+import pytest
+
+class Test_Demo():
+    def test_demo(self):
+        a=5
+        b=-1
+        assert a!=b
+        print("第一个测试demo")
---
+++ test_parametrize.py
@@ -0,0 +1,28 @@
+import pytest
+def add_function(a,b):
+    return a+b
+
+# def test1_add1():
+#     assert add_function(1,1)==2
+# def test1_add2():
+#     assert add_function(1,-1)==0
+# def test1_add3():
+#     assert add_function(1,-1)==-2
+# def test1_add4():
+#     assert add_function(1000,10000)==2
+
+# 参数化，参数加别名，ids
+# @pytest.mark.parametrize("a,b,expected",
+#                         [(3,5,8),
+#                         (-1,-3,-4),
+#                         (10000,1000,11000),
+#                         ],ids=["int","minus","bigint"])
+# def test_add(a,b,expected):
+#     assert add_function(a,b) == expected
+
+
+# 参数排列组合
+@pytest.mark.parametrize("a",[0,0,200])
+@pytest.mark.parametrize("b",[-2,10000,123])
+def test_foo(a,b):
+    print("测试数据组合：a->%s,b->%s"%(a,b))
